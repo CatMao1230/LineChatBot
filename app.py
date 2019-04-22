b284e8472c44f43a2b70afd104172dd1e4b9bc49
catmao <cat841230@gmail.com>
2019-04-22 21:42:28 +0800

    Basic architecture.

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+import configparser
+from flask import Flask, request, abort
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
+
+app = Flask(__name__)
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
+handler = WebhookHandler(config['line_bot']['Channel_Secret'])
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info('Request body: ' + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'ok'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print(event.message.text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.environ['PORT'])
